bf5d05afd10e53340e1370ced2ca855165380b32
Yaroslav <104340925+iNTRUDER-22@users.noreply.github.com>
2022-12-21 18:02:03 +0500

    Add files via upload

---
+++ advertisement/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def advertisement_list(request, *args, **kwargs):
+    return render(request, 'advertisement/advertisement_list.html', {})
+
+
+def advertisement1(request, *args, **kwargs):
+    return render(request, 'advertisement/advertisement1.html', {})
